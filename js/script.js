322ca08710e78943a7a7288f41895571b39e4fd6
ekaterina-chernykh <ekaterina-chernykh@users.noreply.github.com>
2022-10-06 18:51:59 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -8,10 +8,14 @@
  * Do basic math.
  */
 function doMathClicked() {
-  document.getElementById("calculate area").innerHTML = "<p>6 + 5 = " + (6 + 5) + "</p>"
-  
+  document.getElementById("calculate area").innerHTML =
+    "<p>6 + 5 = " + (6 + 5) + "</p>"
 }
-  document.getElementById("subtract-math").innerHTML = "<p>7 - 3 =  " + (7 - 3) + "</p>"  
-  document.getElementById("multiply-math").innerHTML = "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>" 
-  document.getElementById("divide-math").innerHTML = "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
-  document.getElementById("exponent-math").innerHTML = "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
+document.getElementById("subtract-math").innerHTML =
+  "<p>7 - 3 =  " + (7 - 3) + "</p>"
+document.getElementById("multiply-math").innerHTML =
+  "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>"
+document.getElementById("divide-math").innerHTML =
+  "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
+document.getElementById("exponent-math").innerHTML =
+  "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
