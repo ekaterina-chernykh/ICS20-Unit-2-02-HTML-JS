b07601264be178f7cb9f081655194146db7fe923
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-07 03:41:52 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -8,14 +8,6 @@
  * Do basic math.
  */
 function doMathClicked() {
-  document.getElementById("calculate area").innerHTML =
-    "<p>6 + 5 = " + (6 + 5) + "</p>"
+  document.getElementById("calculate-area").innerHTML = "<p>The area is: " + (5 * 3) + "cm^2</p>"  
+  document.getElementById("calculate-perimeter").innerHTML = "<p>The perimeter is: " + (2 * (5 + 3)) + "cm</p>" 
 }
-document.getElementById("subtract-math").innerHTML =
-  "<p>7 - 3 =  " + (7 - 3) + "</p>"
-document.getElementById("multiply-math").innerHTML =
-  "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>"
-document.getElementById("divide-math").innerHTML =
-  "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
-document.getElementById("exponent-math").innerHTML =
-  "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
